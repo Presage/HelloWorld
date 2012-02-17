336e783cc5ead229ba9dd2f4e1e56ce219aed8b8
Sam Macbeth <sm1106@imperial.ac.uk>
2012-02-17 23:32:01 +0000

    Using new method for binding environment services provided to participants.

--- src/main/java/uk/ac/imperial/presage2/helloworld/HelloEnvironment.java
+++ src/main/java/uk/ac/imperial/presage2/helloworld/HelloEnvironment.java
@@ -1,26 +1,13 @@
 package uk.ac.imperial.presage2.helloworld;
 
-import java.util.HashSet;
-import java.util.Set;
-
-import org.apache.log4j.Logger;
-
-import uk.ac.imperial.presage2.core.environment.EnvironmentRegistrationRequest;
-import uk.ac.imperial.presage2.core.environment.EnvironmentService;
-import uk.ac.imperial.presage2.core.environment.SharedStateAccessException;
 import uk.ac.imperial.presage2.core.environment.SharedStateStorage;
-import uk.ac.imperial.presage2.core.environment.UnavailableServiceException;
 import uk.ac.imperial.presage2.util.environment.AbstractEnvironment;
-import uk.ac.imperial.presage2.util.location.ParticipantLocationService;
 import uk.ac.imperial.presage2.util.location.area.Area;
-import uk.ac.imperial.presage2.util.location.area.AreaService;
 import uk.ac.imperial.presage2.util.location.area.HasArea;
 
 import com.google.inject.Inject;
 
 public class HelloEnvironment extends AbstractEnvironment implements HasArea {
-
-	final private Logger logger = Logger.getLogger(HelloEnvironment.class);
 
 	final private Area simArea;
 
@@ -28,26 +15,6 @@ public class HelloEnvironment extends AbstractEnvironment implements HasArea {
 	public HelloEnvironment(Area simArea, SharedStateStorage sharedState) {
 		super(sharedState);
 		this.simArea = simArea;
-	}
-
-	@Override
-	protected Set<EnvironmentService> generateServices(
-			EnvironmentRegistrationRequest request) {
-		final Set<EnvironmentService> services = new HashSet<EnvironmentService>();
-		try {
-			services.add(new ParticipantLocationService(request
-					.getParticipant(), this.sharedState, this));
-			services.add(this.getEnvironmentService(AreaService.class));
-		} catch (SharedStateAccessException e) {
-			logger.warn(
-					"Unable to add ParticipantLocationService to services for participant "
-							+ request.getParticipantID()
-							+ ", error accessing shared state.", e);
-		} catch (UnavailableServiceException e) {
-			logger.warn("Unable to add AreaService to services for participant"
-					+ request.getParticipantID(), e);
-		}
-		return services;
 	}
 
 	public Area getArea() {
--- src/main/java/uk/ac/imperial/presage2/helloworld/HelloModule.java
+++ src/main/java/uk/ac/imperial/presage2/helloworld/HelloModule.java
@@ -3,14 +3,13 @@ package uk.ac.imperial.presage2.helloworld;
 import java.util.HashSet;
 import java.util.Set;
 
-import uk.ac.imperial.presage2.core.environment.ActionHandler;
-import uk.ac.imperial.presage2.core.environment.EnvironmentService;
 import uk.ac.imperial.presage2.core.network.NetworkConstraint;
 import uk.ac.imperial.presage2.core.plugin.Plugin;
 import uk.ac.imperial.presage2.util.environment.AbstractEnvironmentModule;
 import uk.ac.imperial.presage2.util.location.LocationService;
 import uk.ac.imperial.presage2.util.location.LocationStoragePlugin;
 import uk.ac.imperial.presage2.util.location.MoveHandler;
+import uk.ac.imperial.presage2.util.location.ParticipantLocationService;
 import uk.ac.imperial.presage2.util.location.area.AreaService;
 import uk.ac.imperial.presage2.util.location.area.HasArea;
 import uk.ac.imperial.presage2.util.network.NetworkModule;
@@ -23,18 +22,16 @@ public class HelloModule extends AbstractModule {
 
 	@Override
 	protected void configure() {
-		// environment services
-		Set<Class<? extends EnvironmentService>> environmentServices = new HashSet<Class<? extends EnvironmentService>>();
-		environmentServices.add(LocationService.class);
-		environmentServices.add(AreaService.class);
-
-		// action handlers
-		Set<Class<? extends ActionHandler>> actionHandlers = new HashSet<Class<? extends ActionHandler>>();
-		actionHandlers.add(MoveHandler.class);
 
 		// install abstract environment module
-		install(new AbstractEnvironmentModule(HelloEnvironment.class,
-				environmentServices, actionHandlers));
+		install(new AbstractEnvironmentModule(HelloEnvironment.class)
+				.addActionHandler(MoveHandler.class)
+				.addGlobalEnvironmentService(LocationService.class)
+				.addGlobalEnvironmentService(AreaService.class)
+				.addParticipantEnvironmentService(
+						ParticipantLocationService.class)
+				.addParticipantGlobalEnvironmentService(AreaService.class));
+
 		// bind area for handlers/services
 		bind(HasArea.class).to(HelloEnvironment.class);
 
