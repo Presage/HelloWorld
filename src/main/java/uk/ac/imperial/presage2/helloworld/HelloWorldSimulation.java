293a473ca9f0ec42597a3342140919eca3f1a6bf
Sam Macbeth <sm1106@imperial.ac.uk>
2012-05-28 14:10:38 +0100

    Update to 0.3.2 compatibility. Following recommended structure for this version.

--- src/main/java/uk/ac/imperial/presage2/helloworld/HelloWorldSimulation.java
+++ src/main/java/uk/ac/imperial/presage2/helloworld/HelloWorldSimulation.java
@@ -3,14 +3,23 @@ package uk.ac.imperial.presage2.helloworld;
 import java.util.HashSet;
 import java.util.Set;
 
+import uk.ac.imperial.presage2.core.network.NetworkConstraint;
 import uk.ac.imperial.presage2.core.participant.Participant;
+import uk.ac.imperial.presage2.core.plugin.PluginModule;
 import uk.ac.imperial.presage2.core.simulator.InjectedSimulation;
 import uk.ac.imperial.presage2.core.simulator.Parameter;
 import uk.ac.imperial.presage2.core.simulator.Scenario;
-import uk.ac.imperial.presage2.core.simulator.SimulatorModule;
 import uk.ac.imperial.presage2.core.util.random.Random;
+import uk.ac.imperial.presage2.util.environment.AbstractEnvironmentModule;
 import uk.ac.imperial.presage2.util.location.Location;
+import uk.ac.imperial.presage2.util.location.LocationService;
+import uk.ac.imperial.presage2.util.location.LocationStoragePlugin;
+import uk.ac.imperial.presage2.util.location.MoveHandler;
+import uk.ac.imperial.presage2.util.location.ParticipantLocationService;
 import uk.ac.imperial.presage2.util.location.area.Area;
+import uk.ac.imperial.presage2.util.location.area.AreaService;
+import uk.ac.imperial.presage2.util.network.NetworkModule;
+import uk.ac.imperial.presage2.util.network.NetworkRangeConstraint;
 
 import com.google.inject.AbstractModule;
 
@@ -32,9 +41,26 @@ public class HelloWorldSimulation extends InjectedSimulation {
 	@Override
 	protected Set<AbstractModule> getModules() {
 		Set<AbstractModule> modules = new HashSet<AbstractModule>();
-		modules.add(SimulatorModule.multiThreadedSimulator(4));
-		modules.add(new HelloModule());
+
 		modules.add(Area.Bind.area2D(xSize, ySize));
+
+		// install abstract environment module
+		modules.add(new AbstractEnvironmentModule()
+				.addActionHandler(MoveHandler.class)
+				.addGlobalEnvironmentService(LocationService.class)
+				.addGlobalEnvironmentService(AreaService.class)
+				.addParticipantEnvironmentService(
+						ParticipantLocationService.class)
+				.addParticipantGlobalEnvironmentService(AreaService.class));
+
+		// network
+		Set<Class<? extends NetworkConstraint>> constraints = new HashSet<Class<? extends NetworkConstraint>>();
+		constraints.add(NetworkRangeConstraint.class);
+		modules.add(NetworkModule.constrainedNetworkModule(constraints)
+				.withNodeDiscovery());
+
+		modules.add(new PluginModule().addPlugin(LocationStoragePlugin.class));
+
 		return modules;
 	}
 
